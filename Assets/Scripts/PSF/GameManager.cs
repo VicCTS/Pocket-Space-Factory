60d9effe17875a13f32e8e14149ce60fea6348f0
VicCTS <95314178+VicCTS@users.noreply.github.com>
2021-12-12 20:03:34 +0100

    Contador FPS

--- Assets/Scripts/PSF/GameManager.cs
+++ Assets/Scripts/PSF/GameManager.cs
@@ -120,10 +120,8 @@ public class GameManager : MonoBehaviour
            Debug.Log("PUNTUACIÓMAXIMAAQUESTNIVELL: "+ Global.maxScore);
            Debug.Log("NIVELLACTUAL: "+ num);
             Global.requestBox.Add(2);
-            Global.requestBox.Add(2);
-            Global.requestBox.Add(2);
-            Global.requestBox.Add(3);
-            Global.requestBox.Add(1);
+            //Global.requestBox.Add(2);
+            //Global.requestBox.Add(2);
 
             Global.machine1BoxTime = 25;
             Global.machine2BoxTime = 10;
